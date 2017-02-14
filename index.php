8ce8fe8fce73f6f950b5e58ebe033b5c77016bcc
Anna <mahovskie@mail.ru>
2017-02-14 02:05:28 +0200

    add change menu

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <a href="php_basics_tasks/index.php">php_basics_tasks</a><br>
+        <a href="arrays_loops_tasks/index.php">arrays_loops_tasks</a><br>
+    </body>
+</html>
