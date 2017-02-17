12498c0390f1194f2a35f3c142e8c13a42eaecd5
Anna <mahovskie@mail.ru>
2017-02-17 12:12:36 +0200

    add task1

---
+++ arrays_tasks/task_1.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Check if a number is prime number</title>
+</head>
+<body>
+    
+    <h1>Check if a number is prime number</h1>
+    <h3>TODO: remove 'required'. Add php validation</h3>
+    <form method='post'>
+        <input type="text" name="number" />
+        <button>Go</button>
+    </form>
+    
+    
+    <?php 
+        if ($_POST) { 
+            if (!is_numeric($_POST['number'])){
+                die('not a number');
+            } 
+            
+            $number = $_POST['number'];
+            for($i=0, $len = strlen($number); $i< $len; $i++){
+                if($number[$i]=='.')
+                    die('not integer number');
+            }
+            
+            $number = (int)$number;
+            if($number<2) {
+                die('number must be more than 2');
+            }
+            
+            $result = true;
+            
+            if ($number == 2) {
+                $result = true;
+            } else {
+                for ($i = 2; $i < $number; $i++) {
+                    if ($number % $i == 0) {
+                        $result = false;
+                        break;
+                    }
+                }
+            }
+            
+            echo  "Result: " . ($result ? '' : 'Not ') . 'prime';
+        }  
+    ?>
+    
+    
+</body>
+</html>
