77f1c6db02fb0805ab3ed78a54504717fc0feaa9
Anna <mahovskie@mail.ru>
2017-02-21 16:09:25 +0200

    index change

--- index.php
+++ index.php
@@ -7,5 +7,7 @@
     <body>
         <a href="php_basics_tasks/index.php">php_basics_tasks</a><br>
         <a href="arrays_loops_tasks/index.php">arrays_loops_tasks</a><br>
+        <a href="arrays_tasks/index.php">arrays_tasks</a><br>
+        <a href="devionity_hw_06_02/index.php">devionity_hw_all</a><br>
     </body>
 </html>
